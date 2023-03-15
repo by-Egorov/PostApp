72882e36b8f604bd4b6c05ecb35993faa1359f89
egorov.dev <egorov.dev@gmail.com>
2023-03-16 00:36:54 +0300

    натсройка//..

--- src/App.js
+++ src/App.js
@@ -1,49 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './styles/App.css'
-import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
-import MyButton from "./components/Ui/button/MyButton";
-import MyInput from "./components/Ui/input/MyInput";
+import PostForm from "./components/PostForm";
 
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'JavaScript 1', body: 'JavaScript - язык программирования'},
-        {id: 2, title: 'JavaScript 2', body: 'JavaScript - язык программирования'},
-        {id: 3, title: 'JavaScript 3', body: 'JavaScript - язык программирования'}
-    ])
+	const [posts, setPosts] = useState([
+		{ id: 1, title: 'JavaScript 1', body: 'JavaScript - язык программирования' },
+		{ id: 2, title: 'JavaScript 2', body: 'JavaScript - язык программирования' },
+		{ id: 3, title: 'JavaScript 3', body: 'JavaScript - язык программирования' }
+	])
 
-    const [post, setPost] = useState({title: '', body: ''});
+	const createPost = (newPost) => {
+		setPosts([...posts, newPost])
+	}
 
-    const addNewPost = (e) => {
-        e.preventDefault()
-        setPosts([...posts, {...post, id: Date.now()}])
-        setPost({title: '', body: ''})
-    }
+	const removePost = (post) => {
+		setPosts(posts.filter(p => p.id !== post.id))
+	}
 
-    return (
-        <div className='App'>
-            <form>
-                <MyInput
-                    value={post.title}
-                    onChange={e => setPost ({...post, title: e.target.value})}
-                    type="text"
-                    placeholder='Название поста'/>
+	return (
+		<div className='App'>
+			<PostForm create={createPost} />
+			{posts.length !== 0
+				? <PostList remove={removePost} posts={posts} title={"Список постов 1"} />
+				: <div className='post_not_found'>
+					Посты не найдены!
+					<span>
+						:/
+					</span>
+				</div>
+			}
 
-                <MyInput
-                    value={post.body}
-                    onChange={e => setPost ({...post, body: e.target.value})}
-                    type="text"
-                    placeholder='Описание поста'/>
-
-                <MyButton onClick={addNewPost}>
-                    Создать пост
-                </MyButton>
-            </form>
-            <PostList posts={posts} title={"Список постов 1"}/>
-        </div>
-    );
+		</div>
+	);
 }
 
 export default App;
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MyButton from "./Ui/button/MyButton";
 
 const PostItem = (props) => {
 
@@ -11,10 +12,12 @@ const PostItem = (props) => {
                 </div>
             </div>
             <div className='post__btns'>
-                <button>Удалить</button>
+                <MyButton
+                onClick={() => props.remove(props.post)}>
+                    Удалить
+                </MyButton>
             </div>
         </div>
-
     );
 };
 
